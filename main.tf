1e811d748b3d8ba030be9027d2cf06e4564eb8a4
Dwijen Bhattacharjee <dwijen.bapi@gmail.com>
2019-02-15 00:18:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ provider "aiven" {
 
 # Project (Current evaluation credit applicable to Muneer-d23d project , so we can't use the credit to create another
 # Project. This line is commented now. Once we sign the contract then we can use this resource to create new project
-resource "aiven_project" "myproject" {
-    project = "myprojectdwijen"
-}
+#resource "aiven_project" "myproject" {
+#    project = "myprojectdwijen"
+#}
 
 ######################################################################################
 ##            RESOURCES
